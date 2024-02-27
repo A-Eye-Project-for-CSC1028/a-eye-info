7dca8093c68ed723cd88c709a3575c3a2657f3d3
Aidan Juma <aidan@aidanjuma.dev>
2024-02-27 18:35:50 +0000

    ✨ reconstructed + improved tutorial

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+import Image from "next/image";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: (
+    <Image
+      src="/favicon.ico"
+      width={32}
+      height={32}
+      style={{ borderRadius: "9999px" }}
+      alt="Image of a stylised emoticon face - the a-eye logo."
+      priority
+    />
+  ),
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/A-Eye-Project-for-CSC1028/a-eye-info",
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
-}
+};
 
-export default config
+export default config;
